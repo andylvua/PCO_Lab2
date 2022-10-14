ffae1d4d24608e284ca86f9a05b9dd8174a1238c
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-10-14 15:12:55 +0300

    Improved `compile.sh` script
    Updated script logic. Now the libraries will not be recompiled if they are up-to-date with source files.

--- compile.sh
+++ compile.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
+# This script compiles the source code and creates the executable file
 
+# echo settings
 RED="\033[31m"
 NORMAL="\033[0m"
+
 # List of valid arguments
 valid_args=(-h --help -cr --compile-run -c --compile -r --run --clean)
+
+# Variables
+lib_dir=library
+bin_dir=bin
+obj_dir=obj
+dependencies=(mystring.cpp mystring.h)
+
+# Compiler
+cxx=g++
 
 Help(){
     echo "Usage: compile.sh [options]"
@@ -16,30 +28,58 @@ Help(){
 }
 
 CompileLibraries(){
-    mkdir -p bin && mkdir -p obj
+    mkdir -p $bin_dir && mkdir -p $obj_dir
 
     echo "Compiling static library..."
-    g++ -c ./library/mystring.cpp -o ./obj/mystring.o
-    ar rcs ./bin/libmystring.a ./obj/mystring.o
-    echo "Compiled successfully. Written to ./bin/libmystring.a"
+    $cxx -c $lib_dir/mystring.cpp -o $obj_dir/mystring.o
+    ar rcs $bin_dir/libmystring.a $obj_dir/mystring.o
+    echo "Compiled successfully. Written to $bin_dir/libmystring.a"
 
     echo "Compiling dynamic library..."
-    g++ -c ./library/mystring.cpp -fPIC -o ./obj/mystring.o
-    g++ -shared -fPIC -o ./bin/libmystring.so ./obj/mystring.o
-    echo "Compiled successfully. Written to ./bin/libmystring.so"
+    $cxx -c $lib_dir/mystring.cpp -fPIC -o $obj_dir/mystring.o
+    $cxx -shared -fPIC -o $bin_dir/libmystring.so $obj_dir/mystring.o
+    echo "Compiled successfully. Written to $bin_dir/libmystring.so"
 }
 
 CompileExample(){
     echo "Compiling example..."
-    mkdir -p bin && mkdir -p obj
-    g++ -c ./examples/example.cpp -o ./obj/example.o
+    mkdir -p $bin_dir && mkdir -p $obj_dir
+    $cxx -c ./examples/example.cpp -o $obj_dir/example.o
     echo "Linking with dynamic library..."
-    g++ -fPIC ./obj/example.o ./bin/libmystring.so -o ./bin/example
-    echo "Successfully linked with dynamic library. Written to ./bin/example"
+    $cxx -fPIC $obj_dir/example.o $bin_dir/libmystring.so -o $bin_dir/example
+    echo "Successfully linked with dynamic library. Written to $bin_dir/example"
+}
+
+Compile(){
+    if CheckUpToDate; then
+        echo -e "${RED}Libraries are up to date. Nothing to compile${NORMAL}"
+        echo "Do you want to clean-compile? (y/n)"
+        read -r answer
+        if [ "$answer" == "y" ]; then
+            Clean
+            Compile
+        fi
+        return 0
+    fi
+
+    CompileLibraries
+    CompileExample
+}
+
+CheckUpToDate(){
+    if ! CheckCompiled; then
+        return 1
+    fi
+
+    for file in "${dependencies[@]}"; do
+        if [ $lib_dir/"$file" -nt $bin_dir/libmystring.so ]; then
+            return 1
+        fi
+    done
 }
 
 CheckCompiled(){
-    if [[ -f ./bin/example && -f ./bin/libmystring.so ]]; then
+    if [[ -f $bin_dir/example && -f $bin_dir/libmystring.so ]]; then
         return 0
     else
         return 1
@@ -51,11 +91,11 @@ Run(){
         if [[ "$OSTYPE" == "linux-gnu"* ]]; then
             echo "Linux detected. Setting LD_LIBRARY_PATH..."
             echo ""
-            LD_LIBRARY_PATH="./bin" ./bin/example
+            LD_LIBRARY_PATH="$bin_dir" $bin_dir/example
         elif [[ "$OSTYPE" == "darwin"* ]]; then
             echo "MacOS detected. Setting DYLD_LIBRARY_PATH..."
             echo ""
-            DYLD_LIBRARY_PATH="./bin" ./bin/example
+            DYLD_LIBRARY_PATH="$bin_dir" $bin_dir/example
         fi
     else
         echo -e "${RED}Error: nothing to run. Necessary files are not compiled. Run with -c or --compile option to compile.${NORMAL}
@@ -63,8 +103,7 @@ Run(){
 Do you want to automatically compile libraries and run the example? (y/n)"
         read -r answer
         if [[ "$answer" == "y" ]]; then
-            CompileLibraries
-            CompileExample
+            Compile
             Run
         else
             echo "Exiting..."
@@ -77,14 +116,13 @@ Clean(){
     echo "This will remove all compiled files along with the library. Are you sure? (y/n)"
     read -r answer
     if [[ "$answer" == "y" ]]; then
-        rm -rf ./bin
-        rm -rf ./obj
+        rm -rf $bin_dir
+        rm -rf $obj_dir
         echo "Cleaned successfully."
     else
         echo -e "${RED}Aborted${NORMAL}"
     fi
 }
-
 
 # Check if there is no argument
 if [ $# -eq 0 ]; then
@@ -113,15 +151,13 @@ fi
 
 # Check if the argument is -c or --compile
 if [ "$1" = "-c" ] || [ "$1" = "--compile" ]; then
-    CompileLibraries
-    CompileExample
+    Compile
     exit 0
 fi
 
 # Check if the argument is -cr or --compile-run
 if [ "$1" = "-cr" ] || [ "$1" = "--compile-run" ]; then
-    CompileLibraries
-    CompileExample
+    Compile
     Run
     exit 0
 fi
